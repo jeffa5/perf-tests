a56821d305d1052f0fa0646417f6c050b09d5cfa
Andrew Jeffery <dev@jeffas.io>
2021-12-09 11:48:47 +0000

    Add clusterloader2 build config to flake

--- flake.nix
+++ flake.nix
@@ -11,15 +11,36 @@
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system}; in
       {
-        devShell = pkgs.mkShell {
-          packages = with pkgs; [
-            go_1_15
-            kind
-            k9s
+        packages.clusterloader2 = pkgs.buildGoModule {
+          pname = "clusterloader2";
+          version = "0.1.0";
+          src = ./clusterloader2;
+          vendorSha256 = "sha256-dk9LdI+VnJlP13FWv1F0xubT9YKu5m9wQEYKTD+nuIw=";
+          postInstall = ''
+            mv $out/bin/cmd $out/bin/clusterloader2
+          '';
+        };
 
-            nixpkgs-fmt
-            rnix-lsp
-          ];
+        defaultPackage = self.packages.${system}.clusterloader2;
+
+        apps.clusterloader2 = flake-utils.lib.mkApp {
+          drv = self.packages.${system}.clusterloader2;
+        };
+
+        defaultApp = self.apps.${system}.clusterloader2;
+
+        devShell = pkgs.mkShell {
+          packages = with pkgs;
+            [
+              go_1_15
+              kind
+              k9s
+
+              self.packages.${system}.clusterloader2
+
+              nixpkgs-fmt
+              rnix-lsp
+            ];
         };
       });
 }
